f9fe8cb6b28daa32fb10ee085d5041848e78c5ea
Ludv1k <157578117+Ludv1k@users.noreply.github.com>
2024-12-10 14:00:23 +0100

    Changed port

--- hosting.py
+++ hosting.py
@@ -18,4 +18,4 @@ def notroot():
 # Run the Flask app
 if __name__ == '__main__':
     # Set the app to be accessible on the network
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
